5494d53c5608b25170202a53b53489aa430932cf
Miranda Vu <xmrnda@gmail.com>
2023-02-13 20:23:39 -0800

    New places can be added

--- rest-rant/controllers/places.js
+++ rest-rant/controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
     res.render('places/index', {places})
@@ -9,9 +10,20 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-  })  
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
+  
 
 let places = [{
     name: 'H-Thai-ML',
