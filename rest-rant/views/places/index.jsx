0acf2540ff45c623a4bc8f340ffd38c85acbe2af
Miranda Vu <xmrnda@gmail.com>
2023-02-13 20:41:47 -0800

    Show route connected

--- rest-rant/views/places/index.jsx
+++ rest-rant/views/places/index.jsx
@@ -4,8 +4,12 @@ const Def = require('../default')
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
-          <h2>{place.name}</h2>
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${index}`}>
+              {place.name}
+            </a>
+          </h2>
           <p className="text-center">
             {place.cuisines}
           </p>
